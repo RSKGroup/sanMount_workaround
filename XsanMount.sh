499ef9ea2ec117e36eb9cf025096a4066dc90803
ehemmete <eric@sneakypockets.com>
2023-12-15 10:02:24 -0600

    Update XsanMount.sh
    Added comments

--- XsanMount.sh
+++ XsanMount.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# get the /dev/disk# of the Apple_Xsan volume, ignoring all the LUNs (Apple_Xsan_Component)  
 diskNumber=$(diskutil list | awk '/dev/ { disk=$1 } $0~/ Apple_Xsan / { print disk; exit}')
+# get the Xsan volume name from the same source
 diskName=$(diskutil list | awk '$0~/ Apple_Xsan / { print $3}')
 
+# make the mount point in /Volumes for the volume name
 mkdir /Volumes/"$diskName"
+# mount the disk# to the mount point.  Be sure to add any addition options your volume requires
 /System/Library/Filesystems/acfs.fs/Contents/bin/mount_acfs -o nofollow "$diskNumber" /Volumes/"$diskName"
