ce2a6e8bdcaf8927c55720dd9c3a92caf2e3035b
ehemmete <eric@sneakypockets.com>
2023-12-18 14:35:07 -0600

    Update XsanMount.sh

--- XsanMount.sh
+++ XsanMount.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # the script must be run as root to create the mount point and mount the volume
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
 fi
 
-# get the /dev/disk# of the Apple_Xsan volume, ignoring all the LUNs (Apple_Xsan_Component)  
-diskNumber=$(diskutil list | awk '/dev/ { disk=$1 } $0~/ Apple_Xsan / { print disk; exit}')
-# get the Xsan volume name from the same source
-diskName=$(diskutil list | awk '$0~/ Apple_Xsan / { print $3}')
+# get the Xsan volume names
+volumeNames=$(diskutil list | awk '/ Apple_Xsan / { print $3 }')
 
-# make the mount point in /Volumes for the volume name
-mkdir /Volumes/"$diskName"
-# mount the disk# to the mount point.  Be sure to add any additional options your volume requires
-/System/Library/Filesystems/acfs.fs/Contents/bin/mount_acfs -o nofollow "$diskNumber" /Volumes/"$diskName"
+# iterate over volumes
+for name in $volumeNames; do
+  # get the /dev/disk# of the Apple_Xsan volumes, ignoring all the LUNs (Apple_Xsan_Component)  
+  number=$(diskutil list | awk "/ Apple_Xsan $name / { print \$6 }")
+  # make the mount point in /Volumes for the volume name
+  mkdir "/Volumes/$name"
+  # mount the disk# to the mount point.  Be sure to add any additional options your volume requires
+  /System/Library/Filesystems/acfs.fs/Contents/bin/mount_acfs -o nofollow "/dev/$number" "/Volumes/$name"
+done
